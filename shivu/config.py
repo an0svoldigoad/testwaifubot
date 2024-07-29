3e0b3c945e7052f3b53e78593acf801e2cf7005e
Dojwidzey <125593458+an0svoldigoad@users.noreply.github.com>
2024-07-29 13:42:16 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -6,14 +6,14 @@ class Config(object):
     sudo_users = "6845325416", "6765826972"
     GROUP_ID = -1002133191051
     TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
+    mongo_url = "mongodb+srv://babawazeer33:ShitOnYourFace@grabthemdb.pxqjwig.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    SUPPORT_CHAT = "anime_kingdom_group_chat"
+    UPDATE_CHAT = "anime_kingdom_group_chat"
+    BOT_USERNAME = "GrabThemBot"
+    CHARA_CHANNEL_ID = "-1001782672183"
+    api_id = 21531216
+    api_hash = "f1ff78d3d0b3eaa825294af918318ac7"
 
     
 class Production(Config):
