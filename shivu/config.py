a419a2423704d9fd261bd1d9943a703436d35b14
Dojwidzey <125593458+an0svoldigoad@users.noreply.github.com>
2024-09-10 16:11:32 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -7,7 +7,7 @@ class Config(object):
     GROUP_ID = -1002133191051
     TOKEN = "6384445315:AAF1exVSDGdykV_GIGhCXmgoSlf6ZWPiQ98"
     mongo_url = "mongodb+srv://gotouhitoriii:7vzXrCihdSZfTtEs@haremgod.rxndc2y.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
+    PHOTO_URL = ["https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2Fdazai-osamu-pfp--61572719898392305%2F&psig=AOvVaw0u6uFI8ZwkyRwOg_Kdq0by&ust=1726050701833000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJjR55iWuIgDFQAAAAAdAAAAABAx"]
     SUPPORT_CHAT = "anime_kingdom_group_chat"
     UPDATE_CHAT = "anime_kingdom_group_chat"
     BOT_USERNAME = "GrabThemBot"
